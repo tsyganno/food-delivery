27b7ed3a7bde54ae4feec6c2e9bb46f11ae17777
tsyganno <tsyganlee@gmail.com>
2023-01-05 20:19:42 +0800

    add dish.html

--- delivery_app/urls.py
+++ delivery_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from delivery_app.views import IndexView, CategoryDetailView
+from delivery_app.views import IndexView, CategoryView, DishView
 
 app_name = 'app'
 urlpatterns = [
     path('categories/', IndexView.as_view(), name='index'),
-    path('categories/<int:pk>/<slug>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/<int:pk>/<slug>/', CategoryView.as_view(), name='category'),
+    path('categories/dishes/<int:pk>/<slug>/', DishView.as_view(), name='dish'),
 ]
--- delivery_app/views.py
+++ delivery_app/views.py
@@ -16,7 +16,7 @@ class IndexView(View):
         return render(request, 'delivery_app/index.html', {'page_obj': page_obj})
 
 
-class CategoryDetailView(View):
+class CategoryView(View):
     """Страница с блюдами, относящимся к определенной категории"""
     def get(self, request, slug, *args, **kwargs):
         dishes = Dish.objects.filter(category_id=self.kwargs['pk'], category__url_category=self.kwargs['slug'])
@@ -24,4 +24,11 @@ class CategoryDetailView(View):
         paginator = Paginator(dishes, 6)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-        return render(request, 'delivery_app/category_detail.html', {'page_obj': page_obj, 'category': category})
+        return render(request, 'delivery_app/category_list.html', {'page_obj': page_obj, 'category': category})
+
+
+class DishView(View):
+    """Страница с конкретным блюдом"""
+    def get(self, request, slug, *args, **kwargs):
+        dish = get_object_or_404(Dish, id=self.kwargs['pk'])
+        return render(request, 'delivery_app/dish.html', {'dish': dish})
